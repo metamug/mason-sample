710efeba23840a753a85846489d27c86973f3b68
Deepak Mishra <d3ep4k@gmail.com>
2019-05-26 15:48:53 +0530

    added script example

---
+++ WEB-INF/resources/v1.0/script.jsp
@@ -0,0 +1,7 @@
+<jsp:directive.include file="../fragments/mason-init.jspf"/>
+
+<m:resource>
+    <m:request method="GET">
+        <m:script file="test.groovy" />
+    </m:request>
+</m:resource>
--- WEB-INF/scripts/test.groovy
+++ WEB-INF/scripts/test.groovy
@@ -1,24 +1,2 @@
-//@Grab('com.jayway.jsonpath:json-path:2.3.0')
-//@Grab('org.slf4j:slf4j-api')
-//@Grab('org.slf4j:slf4j-simple')
 
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.Configuration;
-
-
-String json = "{\"user\":\"deepak\"}";
-Object document = Configuration.defaultConfiguration().jsonProvider().parse(json);
-
-println JsonPath.read(document, "\$.user");
-
-
-def makethiswork(request, response){
-    return request.param1 + ' ' + response.param1;
-}
-
-
-response.param1 = makethiswork(request, response)
-
-//def process(){
-//    return "Yo!"
-//}
+response['result'] = 'script output'
