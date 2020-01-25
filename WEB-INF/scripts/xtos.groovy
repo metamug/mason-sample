3b067dc6f856e53ea4a6f2393f688d980f8fc554
Anish Hirlekar <anish.hirlekar@metamug.com>
2020-01-25 15:25:48 +0530

    Added xrequest to script

---
+++ WEB-INF/scripts/xtos.groovy
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param _request Incoming MasonRequest Object
+ * @param {var} Output variable for this script set in <m:script file="test.groovy" var="res" /> tag
+ */
+
+//def response = [:];
+def params = _request.getParams();
+
+scriptOut['message'] = 'Hello ' + _context.getAttribute('xreq').getJSONObject('args').getString('foo2');
